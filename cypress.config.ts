b0993f1e6964601a5552db1ce1989b87399588d8
artgrtmnk <78636533+artgrtmnk@users.noreply.github.com>
2023-01-21 18:00:54 +0100

    Framework initialisation, ts, cucumber implemented, preprocessor ready

---
+++ cypress.config.ts
@@ -0,0 +1,60 @@
+import { defineConfig } from "cypress";
+import webpack from "@cypress/webpack-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    webpack({
+      webpackOptions: {
+        resolve: {
+          extensions: [".ts", ".js"],
+        },
+        module: {
+          rules: [
+            {
+              test: /\.ts$/,
+              exclude: [/node_modules/],
+              use: [
+                {
+                  loader: "ts-loader",
+                },
+              ],
+            },
+            {
+              test: /\.feature$/,
+              use: [
+                {
+                  loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                  options: config,
+                },
+              ],
+            },
+          ],
+        },
+      },
+    })
+  );
+
+  return config;
+}
+
+export default defineConfig({
+  e2e: {
+    specPattern: "**/*.feature",
+    setupNodeEvents,
+  },
+  screenshotOnRunFailure: false,
+  chromeWebSecurity: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  waitForAnimations: true,
+  animationDistanceThreshold: 5,
+  defaultCommandTimeout: 5000,
+  pageLoadTimeout: 10000,
+});
